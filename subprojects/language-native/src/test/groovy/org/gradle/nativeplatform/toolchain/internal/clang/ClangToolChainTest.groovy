1da5369b096e338e8c9816ff3a0bf9f3cd89c05f
Adam Murdoch <adam@gradle.com>
2017-07-31 12:37:53 +1000

    Moved the tool chain implementations from platform-native to language-native, as they each drag in support for specific languages.

--- subprojects/language-native/src/test/groovy/org/gradle/nativeplatform/toolchain/internal/clang/ClangToolChainTest.groovy
+++ subprojects/language-native/src/test/groovy/org/gradle/nativeplatform/toolchain/internal/clang/ClangToolChainTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.nativeplatform.toolchain.internal.gcc
+package org.gradle.nativeplatform.toolchain.internal.clang
 
 import org.gradle.api.Action
 import org.gradle.api.internal.file.FileResolver
@@ -25,7 +25,6 @@ import org.gradle.nativeplatform.internal.CompilerOutputFileNamingSchemeFactory
 import org.gradle.nativeplatform.platform.internal.NativePlatformInternal
 import org.gradle.nativeplatform.toolchain.GccPlatformToolChain
 import org.gradle.nativeplatform.toolchain.internal.NativeCompilerFactory
-import org.gradle.nativeplatform.toolchain.internal.clang.ClangToolChain
 import org.gradle.nativeplatform.toolchain.internal.gcc.version.CompilerMetaDataProviderFactory
 import org.gradle.process.internal.ExecActionFactory
 import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider
